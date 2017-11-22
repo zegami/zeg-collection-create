4bafd6912f8f67075c1dd4d60a5a34e7b35cf8c8
Anthony Kveder <anthony.kveder@zegami.com>
2017-11-22 15:24:50 +0000

    Readded documentation

--- zegami_zeg/api.py
+++ zegami_zeg/api.py
@@ -10,7 +10,6 @@ from . import (
 
 
 # Some mimetypes for file upload
-PNG_TYPE = "image/png"
 TSV_TYPE = "text/tab-separated-values"
 XSLT_TYPE = "application/xml"
 
@@ -19,6 +18,7 @@ class Client(object):
     """HTTP client for interacting with the Zegami api."""
 
     def __init__(self, api_url, project, token):
+        """Initialise client."""
         self.api_url = api_url
         self.project = project
         self.token = token
@@ -26,6 +26,7 @@ class Client(object):
         self.session = http.make_session(auth)
 
     def create_collection(self, name, description=None, dynamic=False):
+        """Create a new collection."""
         url = "{}v0/project/{}/collections/".format(self.api_url, self.project)
         info = {"name": name, "dynamic": dynamic}
         if description is not None:
@@ -34,12 +35,14 @@ class Client(object):
         return response_json['collection']
 
     def update_collection(self, coll_id, info):
+        """Update an existing collection."""
         url = "{}v0/project/{}/collections/{}".format(
             self.api_url, self.project, coll_id)
         response_json = http.put_json(self.session, url, info)
         return response_json['collection']
 
     def create_imageset(self, name, source=None):
+        """Create a new Imageset."""
         url = "{}v0/project/{}/imagesets/".format(self.api_url, self.project)
         info = {"name": name}
         if source is not None:
@@ -47,7 +50,8 @@ class Client(object):
         response_json = http.post_json(self.session, url, info)
         return response_json['imageset']
 
-    def create_join(self, name, imageset_id, dataset_id, join_field='id'):
+    def create_join(self, name, imageset_id, dataset_id, join_column="id"):
+        """Join an existing imageset to a dataset."""
         url = "{}v0/project/{}/datasets/".format(
             self.api_url, self.project)
         info = {
@@ -55,26 +59,33 @@ class Client(object):
             "source": {
                 "imageset_id": imageset_id,
                 "dataset_id": dataset_id,
-                "imageset_name_join_to_dataset":
-                    {"dataset_column": join_field},
+                "imageset_name_join_to_dataset": {"dataset_column": join_column},
             }
         }
         response_json = http.post_json(self.session, url, info)
         return response_json["dataset"]
 
     def upload_data(self, dataset_id, name, file):
+        """Upload a data file."""
         url = "{}v0/project/{}/datasets/{}/file".format(
             self.api_url, self.project, dataset_id)
         response_json = http.post_file(self.session, url, name, file, TSV_TYPE)
         return response_json
 
-    def upload_png(self, imageset_id, name, file):
+    def upload_image(self, imageset_id, name, file, mime_type):
+        """Upload an image."""
         url = "{}v0/project/{}/imagesets/{}/images".format(
             self.api_url, self.project, imageset_id)
-        response_json = http.post_file(self.session, url, name, file, PNG_TYPE)
+        response_json = http.post_file(
+            self.session,
+            url,
+            name,
+            file,
+            mime_type)
         return response_json
 
     def upload_zegx(self, collection_id, file):
+        """Upload an XSLT."""
         url = "{}v0/project/{}/collections/{}/zegx".format(
             self.api_url, self.project, collection_id)
         response_json = http.put_file(self.session, url, file, XSLT_TYPE)
